6a0f3c19e8d2399c3479cc5bc44a3940f24bb3cf
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2026-05-09 11:45:43 +0600

    [m-1]problem 3 done

--- module_1.py
+++ module_1.py
@@ -1,9 +1,9 @@
 #Problem: 01
-# num = int(input("Enter a number: "))
-# if(num%2==0):
-#     print("Even number")
-# else:
-#     print("Odd number")
+num = int(input("Enter a number: "))
+if(num%2==0):
+    print("Even number")
+else:
+    print("Odd number")
 
 #Problem: 02
 first_number = int(input("First number: "))
@@ -17,3 +17,9 @@ elif (operator=="*"):
     print(first_number*second_number)
 else:
     print(first_number/second_number)
+
+#Problem: 03
+sum=0
+for i in range(2,101,2):
+    sum=sum+i
+print(sum)
