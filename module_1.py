92299082e15714eee9191cf723dfb0365970d156
ZiadAbdulBari <ziadabdulbari01@gmail.com>
2026-05-09 11:40:54 +0600

    [m-1]problen 2 done

--- module_1.py
+++ module_1.py
@@ -1,6 +1,19 @@
 #Problem: 01
-num = int(input("Enter a number "))
-if(num%2==0):
-    print("Even number")
+# num = int(input("Enter a number: "))
+# if(num%2==0):
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+#Problem: 02
+first_number = int(input("First number: "))
+second_number = int(input("Second number: "))
+operator = input("Take an operator: ")
+if(operator=="+"):
+    print(first_number+second_number)
+elif(operator=="-"):
+    print(first_number-second_number)
+elif (operator=="*"):
+    print(first_number*second_number)
 else:
-    print("Odd number")
+    print(first_number/second_number)
